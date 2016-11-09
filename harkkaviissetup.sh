b0e309efceb1d9f6d19953d76f34e97be01ea8c0
Niko Söderholm LapTop <A0402553@myy.haaga-helia.fi>
2016-11-09 07:11:29 +0000

    Modified harkkaviissetup.sh to work on live-usb

--- harkkaviissetup.sh
+++ harkkaviissetup.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y puppet
 sudo mkdir /etc/puppet/modules/iamyourmaster
 cd /etc/puppet/modules/iamyourmaster
 sudo mkdir manifests templates
-cd /tmp/harkkaviis
-#cd /home/xubuntu/Code/harkkaviis
+#cd /tmp/harkkaviis
+cd /home/xubuntu/code/harkkaviis
 sudo cp iamyourmaster /etc/puppet/modules/iamyourmaster/manifests/init.pp
 sudo puppet apply -e 'class { iamyourmaster: }'
